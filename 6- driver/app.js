883903b3ff2638e090f3af1973e2f394978b4d1d
Denoria <bulanovaay@yandex.ru>
2025-03-23 18:49:21 +0300

    #удалена ненужная папка

--- 6- driver/app.js
+++
@@ -1,5 +0,0 @@
-const hasLicence = true;
-const age = 20;
-const isDrunk = false;
-const canDrive = age>=18 && hasLicence && !isDrunk;
-console.log(canDrive ? 'Может водить машину' : 'Не может водить машину') 
